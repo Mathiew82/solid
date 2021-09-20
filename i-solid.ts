0757025634f4d9a0042dd208ce6d2255a936ef1f
Mathiew82 <amateo1982@yahoo.es>
2021-09-20 22:26:23 +0200

    Agregados los dos archivos de la I y la D. Agregado el ejemplo de la segregación de interfaces

---
+++ i-solid.ts
@@ -0,0 +1,49 @@
+// En este ejemplo se crean dos interfaces para separar los métodos que solo implementarán las clases de vehículos Tesla
+// Si solo existiera una interfaz se estaría violando el principio de segregación de interfaces, ya que habrían métodos
+// que debería implementar la clase Mustang que no necesitaría.
+interface Car {
+  accelerate: () => void;
+  brake: () => void;
+  startEngine: () => void;
+}
+
+interface Tesla {
+  autoPilot: () => void;
+  ludicrousSpeed: () => void;
+}
+
+class Mustang implements Car {
+  accelerate() {
+    console.log("accelerate");
+  }
+
+  brake() {
+    console.log("brake");
+  }
+
+  startEngine() {
+    console.log("startEngine");
+  }
+}
+
+class tesla implements Car, Tesla {
+  accelerate() {
+    console.log("accelerate");
+  }
+
+  brake() {
+    console.log("brake");
+  }
+
+  startEngine() {
+    console.log("startEngine");
+  }
+
+  autoPilot() {
+    console.log("autoPilot");
+  }
+
+  ludicrousSpeed() {
+    console.log("ludicrousSpeed");
+  }
+}
